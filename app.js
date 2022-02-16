50d83124f82244e41f23853cb5fd4eb3aa0fbe87
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-02-16 14:56:12 +1000

    Week 1. Task 2.

--- app.js
+++ app.js
@@ -1,53 +1,27 @@
-const fs = require('fs');
-const path = require('path');
+const http = require('http');
+const PORT = 3000;
+const INTERVAL = 1000;
+const MAX = 5;
 
-function main(sourceDir, destinationDir) {
-  if (!sourceDir && !destinationDir) {
-    console.log("Missing require parameters\nUsage: app.js sourceDirName destinationDirname ...");
-    return;
-  }
-
-  function createDirAndCallCallback(dir, callback) {
-    fs.stat(dir, (err) => {
-      if (err) {
-        fs.mkdir(dir, () => {
-          if (callback) callback();
-        });
-      } else {
-        if (callback) callback();
-      }
-    })
-  }
-
-  function readDirectory(source) {
-    fs.readdir(source, { withFileTypes: true }, (err, files) => {
-      files.forEach(file => {
-        if (file.isDirectory()) {
-          readDirectory(path.join(source, file.name));
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    let interval;
+    let counter = 0;
+    res.write("", () => {
+      interval = setInterval(() => {
+        if (counter < MAX) {
+          console.log(new Date().toUTCString())
+          counter++;
         } else {
-          fileCopy(source, file.name);
+          res.end(new Date().toUTCString(), () => {
+            clearInterval(interval);
+          });
         }
-      });
+      }, INTERVAL)
     })
   }
+})
 
-  function fileCopy(source, filename) {
-    const firstLetterOfFile = filename[0].toUpperCase();
-    const finalDestination = path.join(__dirname, destinationDir, firstLetterOfFile);
-    createDirAndCallCallback(finalDestination, () => {
-      fs.copyFile(path.join(source, filename), path.join(finalDestination, filename), (err) => {
-        if (err) throw err;
-      })
-    });
-  }
-
-  createDirAndCallCallback(destinationDir);
-  readDirectory(path.join(__dirname, sourceDir));
-
-  fs.rm(sourceDir, { recursive: true, force: true }, (err) => {
-    if (err) throw err;
-  })
-
-}
-
-main(process.argv[2], process.argv[3]);
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+})
