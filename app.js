f0a1be309b76cda9564cbcaa9b1ac9fc0487421c
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-02-18 14:55:44 +1000

    app is using real time now instead a simple counter

--- app.js
+++ app.js
@@ -6,18 +6,20 @@ const TIME = process.env.time || 3000;
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     let interval;
-    let counter = 0;
+    const startTime = Date.now();
+    let endTime = Date.now();
+
     res.write("", () => {
       interval = setInterval(() => {
-        if (counter < MAX) {
-          console.log(new Date().toUTCString())
-          counter++;
-        } else {
-          res.end(new Date().toUTCString(), () => {
-            clearInterval(interval);
-          });
-        }
-      }, INTERVAL)
+          if ((endTime - startTime) > TIME) {
+            res.end(new Date().toUTCString(), () => {
+              clearInterval(interval);
+            });
+          } else {
+            endTime = Date.now();
+            console.log(new Date().toUTCString())
+          }
+        }, INTERVAL)
     })
   }
 })
