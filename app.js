eba10b894fd06748ef3241713431283c1982382f
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-02-18 15:26:33 +1000

    copy files on promises

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
-const http = require('http');
-const PORT = 3000;
-const INTERVAL = 1000;
-const MAX = 5;
+const fs = require('fs');
+const { resolve } = require('path');
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    let interval;
-    let counter = 0;
-    res.write("", () => {
-      interval = setInterval(() => {
-        if (counter < MAX) {
-          console.log(new Date().toUTCString())
-          counter++;
-        } else {
-          res.end(new Date().toUTCString(), () => {
-            clearInterval(interval);
-          });
-        }
-      }, INTERVAL)
+async function main(sourceDir, destinationDir) {  
+  if (!sourceDir && !destinationDir) {
+    console.log("Missing require parameters\nUsage: app.js sourceDirName destinationDirname ...");
+    return;
+  }
+
+  function createDir(dir) {
+    return new Promise((resolve) => {
+      const item = fs.statSync(dir, {throwIfNoEntry: false});
+      if (!item) {
+        resolve(fs.mkdirSync(dir));
+      } else {
+        resolve(false);
+      }
     })
   }
-})
 
-server.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-})
+  function readDir(dir) {
+    return new Promise((resolve) => {
+      const files = fs.readdirSync(dir, {withFileTypes: true});
+      resolve(files);
+    })
+  }
+
+  async function processFiles(source) {
+    const files = await readDir(source);
+    files.forEach(async (file) => {
+      if (file.isDirectory()) {
+        await processFiles(path.join(source, file.name));
+      } else {
+        await fileCopy(source, file.name);
+      }
+    });
+  }
+
+  async function fileCopy(source, filename) {
+    const firstLetterOfFile = filename[0].toUpperCase();
+    const finalDestination = path.join(__dirname, destinationDir, firstLetterOfFile);
+
+    await createDir(finalDestination);
+
+    return new Promise((resolve) => {
+      resolve(fs.copyFileSync(path.join(source, filename), path.join(finalDestination, filename)));
+    })
+  }
+
+  await createDir(destinationDir);
+  await processFiles(path.join(__dirname, sourceDir));
+
+}
+
+main(process.argv[2], process.argv[3]);
